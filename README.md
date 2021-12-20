3252c43fda38b033d0ec1e8ac871f4163fc28c20
Sanjana <sanjana@itribe.in>
2021-12-20 15:49:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Creating cross browser extensions using WebExtension API and React JS
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+### Testing for extension
+
+Install the project locally like any react project.
+
+To check the extension up and running in browsers(chrome and firefox) run `npm run build` command and paste the
+`manifest.json` file from `extras` to `build` folder.
+
+For Google Chrome -> Select the `build` folder while testing the extension. \
+For Firefox -> Select the `manifest.json` from the `build` folder. \
---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const greetings = {
+  morning: "Good morning",
+  noon: "Good afternoon",
+  evening: "Good evening",
+  night: "Good night",
+};
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      greeting: greetings.morning,
+    };
+    this.startTime = this.startTime.bind(this);
+  }
+
+  componentDidMount() {
+    this.startTime();
+  }
+
+  startTime() {
+    let today = new Date();
+    let h = today.getHours();
+    let greeting;
+    if (h > 6 && h < 12) {
+      greeting = greetings.morning;
+    } else if (h >= 12 && h < 17) {
+      greeting = greetings.noon;
+    } else if (h >= 17 && h < 20) {
+      greeting = greetings.evening;
+    } else {
+      greeting = greetings.night;
+    }
+    this.setState({ greeting });
+    setTimeout(this.startTime, 1000);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.greeting}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
